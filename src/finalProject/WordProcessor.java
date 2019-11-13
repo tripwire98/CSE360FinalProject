b506f9e4203c82bd34f8603364f4983b228d9df8
BrandonO <brandonobier1998@gmail.com>
2019-11-12 21:41:23 -0700

    Test

---
+++ src/finalProject/WordProcessor.java
@@ -0,0 +1,18 @@
+package finalProject;
+import java.util.*;
+
+public class WordProcessor {
+    
+	public void dispMessage()
+    {
+        System.out.println("Hello World.");
+    }
+     
+    //Main method
+    public static void main(String s[])
+    {
+        //creat object of HelloWorld Class
+    	WordProcessor obj = new WordProcessor();
+        obj.dispMessage();
+    }
+}
